67fa6a5d89cafc60dc1b6963c43edff63fd3fca2
github-Maribelll <mashik.b@yandex.ru>
2021-10-23 23:08:50 +0300

    feat: add time functions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
-// index.js
-// создаем элемент заголовка
-const heading = document.createElement("h1");
-heading.textContent = "Как интересно!";
+// time js
+const time = document.querySelector(".time");
+const day = document.querySelector(".date");
+const greeting = document.querySelector(".greeting");
+const myname = document.querySelector(".myname");
 
-// добавляем заголовок в DOM
-const root = document.querySelector("#root");
-root.append(heading);
+//вывод времени
+function showTime() {
+  const date = new Date();
+  const currentTime = date.toLocaleTimeString();
+  time.textContent = currentTime;
+  setTimeout(showTime, 1000); // обновление времени
+  showGreeting();
+  showDate();
+}
+function showDate() {
+  const date = new Date();
+  const options = {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    timeZone: "UTC",
+  };
+  const currentDate = date.toLocaleDateString("en-US", options);
+  day.textContent = currentDate;
+}
+
+showTime();
+
+// вывод дня недели, даты и месяца
+function showDate() {
+  const date = new Date();
+  const options = {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    timeZone: "UTC",
+  };
+  const currentDate = date.toLocaleDateString("en-US", options);
+  day.textContent = currentDate;
+}
